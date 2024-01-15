8a009df50470e2e246c31ef7cc4931c4699d8aa7
khadijah40 <116455271+khadijah40@users.noreply.github.com>
2024-01-15 09:18:04 -0800

    Add files via upload

---
+++ chap2.js
@@ -0,0 +1,27 @@
+let name='khadijah';
+let age=19;
+let yes = true;
+console.log("My name is" + name + "and my age is "+ age + "and I can code java script" + yes);
+
+
+
+
+let a=3;
+let b=4;
+let c=7;
+console.log(a+b);
+console.log(Math.round(a/c));
+c=c%b;
+console.log(c);
+
+let miles = prompt("enter the value want to change ");
+result=a*1.609;
+console.log("The distance of" + miles  + "kms is equal to" + result +  "miles");
+
+
+
+//BMI calculator
+let w=prompt("enter weight in kilograms");
+let h=prompt("enter height in meters");
+let BMI =w/(h**2);
+console.log("Your BMI is: "+BMI);
